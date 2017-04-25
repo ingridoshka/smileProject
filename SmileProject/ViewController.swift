29071a4f2a31d219173b83e863ee4b265baaf0b6
Ingridoshka <Ingridoshka@MacBooks-MacBook-Pro.local>
2017-04-25 16:59:21 +0300

    Initial Commit

---
+++ SmileProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SmileProject
+//
+//  Created by Ingridoshka on 2017-04-25.
+//  Copyright © 2017 MacBook Retina 2014 256Gb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
